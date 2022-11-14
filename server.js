c1e9a2772b6164eba7b435d71e20839c8c0c0931
tfermoso <tomas.fermoso@gmail.com>
2022-11-14 17:21:36 +0100

    Clase conexion

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express=require('express');
+const bodyParser=require('body-parser');
 const session = require('express-session');
 const ejs =require('ejs');
 require('dotenv').config();
 
+
 const app=express();
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(session({
+    secret: '5577-4D44-WppQ38S',
+    resave: true,
+    saveUninitialized: true
+}));
 
 app.get("/login",(req, res)=>{
     res.render('login.ejs');
+})
+app.post("/login",(req,res)=>{
+    
+    res.send("ok")
 })
 
 
